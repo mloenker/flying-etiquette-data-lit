d6cee659cdd7de97fa02f7dc32707e10337ce26c
Ömer Akgeyik <m.o.akgeyik@gmail.com>
2024-01-12 18:03:44 +0100

    added shortened answers

--- features_trans_back.py
+++ features_trans_back.py
@@ -147,6 +147,154 @@ def features_trans_back(feature, number=None):
         return return_feature(armrest_three_map, number)
     
     
+def features_trans_back_shoretened(feature, number=None):
+    '''
+    This function turns the transformed answers shortened back to the original one.
+    Args: 
+        - Feature: The Feature (Question) you refer to
+        - number: The corresponding answer you need. 
+                  Default is None.
+                  If None is given whole transform of the feature will be given.
+    '''
+
+    if feature == 'On a 6 hour flight from NYC to LA, how many times is it acceptable to get up if you\'re not in an aisle seat?':
+        times_map = {
+            '>5': 0,
+            '4': 1,
+            '3': 2,
+            '2': 3,
+            '1': 4,
+            '0': 5
+            }
+        return return_feature(times_map, number)
+
+
+    if feature == 'Who should have control over the window shade?':
+        shade_control_map = {
+            'Person in window seat': 0,
+            'Everyone in row': 1
+        }
+        return return_feature(shade_control_map, number)
+    
+    if feature == 'Under normal circumstances, does a person who reclines their seat during a flight have any obligation to the person sitting behind them?':
+        recline_obligation_map = {
+            'Yes': 1,
+            'No the person on the flight has no obligation to the person behind them': 0
+        }
+        return return_feature(recline_obligation_map, number)
+    
+    if feature=='Gender':
+        gender_map = {'Male': 0, 'Female': 1}
+        return return_feature(gender_map, number)
+    
+    if feature == 'Age':
+        age_map = {'18-29': 0, '30-44': 1, '45-60': 2, '> 60': 3}
+        return return_feature(age_map, number)
+    
+    if feature=='Do you have any children under 18?':
+        binary_map = {'Yes': 1, 'No': 0}
+        return return_feature(binary_map, number)
+    
+    if feature=='Have you ever used personal electronics during take off or landing in violation of a flight attendant\'s direction?':
+        binary_map = {'Yes': 1, 'No': 0}
+        return return_feature(binary_map, number)
+    
+    if feature=='Have you ever smoked a cigarette in an airplane bathroom when it was against the rules?':
+        binary_map = {'Yes': 1, 'No': 0}
+        return return_feature(binary_map, number)
+ 
+    if feature== 'Given the opportunity, would you eliminate the possibility of reclining seats on planes entirely?':
+        binary_map = {'Yes': 1, 'No': 0}
+        return return_feature(binary_map, number)
+    
+    if feature == 'Household Income':
+        income_map = {'$0 - $25k': 0, '$25k - $50k': 1, '$50k - $100k': 2, '$100k - $150k': 3, '>150k': 4}
+        return return_feature(income_map, number)
+    
+    if feature=='Education':
+        education_map = {'No high school': 0, 'High school': 1, 'College or similar': 2, 
+                     'Bachelor': 3, 'Graduate': 4}
+        return return_feature(education_map, number)
+
+    if feature=='Location (Census Region)':
+        location_map = {'Pacific': 0, 'East North Central': 1, 'New England': 2, 'Mountain': 3, 'South Atlantic': 4, 
+                    'East South Central': 5, 'Middle Atlantic': 6, 'West North Central': 7, 'West South Central': 8}
+        return return_feature(location_map, number)
+    
+    if feature=='How often do you travel by plane?':
+        travel_frequency_map = {
+            'daily': 5,
+            'byweekly': 4,
+            'weekly': 3,
+            'monthly': 2,
+            'yearly': 1,
+            'Never': 0
+        }
+        return return_feature(travel_frequency_map, number)
+    
+    if feature=='Do you ever recline your seat when you fly?':
+        recline_seat_map = {
+            'Always': 0,
+            'Usually': 1,
+            'Every Second': 2,
+            'Sometimes': 3,
+            'Never': 4
+        }
+        return return_feature(recline_seat_map, number)
+        
+    
+    rudeness_questions = [
+        'Generally speaking, is it rude to say more than a few words tothe stranger sitting next to you on a plane?',
+        'Is it rude to ask someone to switch seats with you in order to be closer to friends?',
+        'Is itrude to ask someone to switch seats with you in order to be closer to family?',
+        'Is it rude to wake a passenger up if you are trying to go to the bathroom?',
+        'Is itrude to wake a passenger up if you are trying to walk around?',
+        'In general, is itrude to bring a baby on a plane?',
+        'In general, is it rude to knowingly bring unruly children on a plane?'
+    ]
+
+    rudeness_map = {
+        'No': 0,
+        'A little bit' : 1,
+        'Yes': 2
+    }
+    for q in rudeness_questions:
+        if feature==q:
+            return return_feature(rudeness_map, number)
+    
+    rudeness_questions2 = ['Is itrude to move to an unsold seat on a plane?',
+                            'Is itrude to recline your seat on a plane?'
+                            ]
+    rudeness_map2 = {
+        'No': 0,
+        'A little bit' : 1,
+        'Yes': 2
+    }
+
+    for q in rudeness_questions2:
+        if feature==q:
+            return return_feature(rudeness_map2, number)
+    
+    if feature=='In a row of two seats, who should get to use the middle arm rest?':
+        armrest_two_map = {
+            'Aisle':5,
+            'Share':4,
+            'Other': 3,
+            'First One': 2,
+            'Window': 1    
+        }
+        return return_feature(armrest_two_map, number)
+    
+    if feature=='In a row of three seats, who should get to use the two arm rests?':
+        armrest_three_map = {
+            'Middle':5,
+            'Share':4,
+            'Other': 3,
+            'First One':2,
+            'Edges':1    
+        }
+        return return_feature(armrest_three_map, number)
+    
 
 def change_dict(dict):
     dict_changed = {}
