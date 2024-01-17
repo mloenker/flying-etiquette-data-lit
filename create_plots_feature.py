edc6ba3d25f2c8a5227b7047a90a79f7cf454757
Ömer Akgeyik <m.o.akgeyik@gmail.com>
2024-01-17 20:01:09 +0100

    added new method to get every feature list

--- create_plots_feature.py
+++ create_plots_feature.py
@@ -8,6 +8,45 @@ class MakeFeaturePlots:
         self.df = df 
         self.feature = feature       
 
+    def list_features(self, get_f = None):
+        '''
+        this returns a list of all features. If number is giving it returns the value (feature) for giving index number
+        params:
+            get_f (int): default is None
+
+        '''
+
+        f_list = ['How often do you travel by plane?',
+                'Do you ever recline your seat when you fly?', 'How tall are you?',
+                'Do you have any children under 18?',
+                'In a row of three seats, who should get to use the two arm rests?',
+                'In a row of two seats, who should get to use the middle arm rest?',
+                'Who should have control over the window shade?',
+                'Is itrude to move to an unsold seat on a plane?',
+                'Generally speaking, is it rude to say more than a few words tothe stranger sitting next to you on a plane?',
+                'On a 6 hour flight from NYC to LA, how many times is it acceptable to get up if you\'re not in an aisle seat?',
+                'Under normal circumstances, does a person who reclines their seat during a flight have any obligation to the person sitting behind them?',
+                'Is itrude to recline your seat on a plane?',
+                'Given the opportunity, would you eliminate the possibility of reclining seats on planes entirely?',
+                'Is it rude to ask someone to switch seats with you in order to be closer to friends?',
+                'Is itrude to ask someone to switch seats with you in order to be closer to family?',
+                'Is it rude to wake a passenger up if you are trying to go to the bathroom?',
+                'Is itrude to wake a passenger up if you are trying to walk around?',
+                'In general, is itrude to bring a baby on a plane?',
+                'In general, is it rude to knowingly bring unruly children on a plane?',
+                'Have you ever used personal electronics during take off or landing in violation of a flight attendant\'s direction?',
+                'Have you ever smoked a cigarette in an airplane bathroom when it was against the rules?',
+                'Gender', 
+                'Age', 
+                'Household Income', 
+                'Education',
+                'Location (Census Region)'
+                ]
+        
+        if get_f is None:
+            return f_list
+        else:
+            return f_list[get_f]
 
     def calculate_conditional_probability(self, changed_df, condition_column, event_column):
         # Group by the condition column and calculate the conditional probabilities
